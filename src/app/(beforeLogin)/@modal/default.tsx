781911bc668d8a067e373246d92f96285fae6ed7
Kaia <kaia.baek@sparkplus.co>
2024-12-19 13:57:14 +0900

    default.tsx

---
+++ src/app/(beforeLogin)/@modal/default.tsx
@@ -0,0 +1,9 @@
+const Default = () => {
+  return null; // 화면에 아무것도 그리지 마라
+};
+
+export default Default;
+
+// FIXME: default.tsx
+// parallel route에 대한 기본값
+// parallel route가 필요 없을 때 사용됨
--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -8,7 +8,11 @@ export default function BeforeLoginLayout({
   return (
     <div>
       {modal}
+      {/* modal -> default.tsx를 작성하지 않으면 modal을 찾을 수가 없기 때문에 404가 발생 */}
       {children}
     </div>
   );
 }
+
+// 주소가 root일 때는 children -> page.tsx, modal -> @modal/default.tsx
+// 주소가 /i/flow/login일 때는 children -> i/flow/login/page.tsx, modal -> @modal/i/flow/login/page.tsx
